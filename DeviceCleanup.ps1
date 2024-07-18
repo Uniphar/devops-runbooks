e4da81b931c144589bba00bd582ac8d960c1b001
Yavar Hasanov <yavar@yavarhasanov.com>
2024-07-18 14:01:21 +0100

    Refactored DeviceCleanup runbook

--- DeviceCleanup.ps1
+++ DeviceCleanup.ps1
@@ -1,155 +1,204 @@
-<#PSScriptInfo
+<#
 .SYNOPSIS
-    Script for Entra ID to cleanup stale device objects
- 
+Performs cleanup of devices based on inactivity thresholds and sends an email report.
+
 .DESCRIPTION
-    This script will get the Entra device objects 
-    The script then compare the ApproximateLastSignInDateTime with the cleanup threshold and remove the device if it is older than the threshold 
-    The script uses Ms Graph with MGGraph modules
-        
-.EXAMPLE
-   .\Entra-Cleanup-StaleDevices.ps1
-    Will cleanup stale devices 
+This script disables/deletes devices that have been inactive for a specified threshold and optionally deletes them. It generates reports and sends an email summary.
 
-.NOTES
-    Bawsed on script written by Mr-Tbone (Tbone Granheden) Coligo AB
+.PARAMETER SendGridApiKeyKvName
+The name of the Key Vault instance containing the SendGrid API key.
 
-.CHANGELOG
-    1.0. - Initial Version
+.PARAMETER SendGridApiKeyKvSecretName
+The name of the secret in the Key Vault containing the SendGrid API key.
+
+.PARAMETER SendGridSenderEmailAddress
+The email address of the sender.
+
+.PARAMETER SendGridRecipientEmailAddresses
+An array of recipient email addresses.
+
+.PARAMETER SendGridApiEndpoint
+The endpoint URL for the SendGrid API.
+
+.PARAMETER DeviceDisableThreshold
+Number of inactive days to determine a stale device to disable.
+
+.PARAMETER DeviceDeleteThreshold
+Number of inactive days to determine a stale device to delete.
+
+.PARAMETER ScriptAction
+The action to perform on the devices. Options are "ReportOnly", "DisableAndDelete".
 #>
 
-#region ---------------------------------------------------[Set script requirements]-----------------------------------------------
-#
-#Requires -Modules Microsoft.Graph.Authentication
-#Requires -Modules Microsoft.Graph.identity.DirectoryManagement
-#
-#endregion
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $SendGridApiKeyKvName,
 
-#region ---------------------------------------------------[Script Parameters]-----------------------------------------------
-#endregion
+    [Parameter(Mandatory = $true)]
+    [string] $SendGridApiKeyKvSecretName,
 
-#region ---------------------------------------------------[Modifiable Parameters and defaults]------------------------------------
-# Customizations
-[int]$DeviceDisableThreshold = 90        # Number of inactive days to determine a stale device to disable
-[int]$DeviceDeleteThreshold  = 180        # Number of inactive days to determine a stale device to delete
-[Bool]$TestMode             = $true    # $True = No devices will be deleted, $False = Stale devices will be deleted
-[Bool]$Verboselogging       = $True     # $True = Enable verbose logging for t-shoot. $False = Disable Verbose Logging
-#endregion
+    [Parameter(Mandatory = $true)]
+    [string] $SendGridSenderEmailAddress,
 
-#region ---------------------------------------------------[Set global script settings]--------------------------------------------
-Set-StrictMode -Version Latest
-#endregion
+    [Parameter(Mandatory = $true)]
+    [string[]] $SendGridRecipientEmailAddresses,
 
-#region ---------------------------------------------------[Import Modules and Extensions]-----------------------------------------
-import-Module Microsoft.Graph.Authentication
-import-Module Microsoft.Graph.identity.DirectoryManagement
-#endregion
+    [Parameter(Mandatory = $false)]
+    [string] $SendGridApiEndpoint = "https://api.sendgrid.com/v3/mail/send",
 
-#region ---------------------------------------------------[Static Variables]------------------------------------------------------
-[System.Collections.ArrayList]$RequiredScopes   = @("Device.ReadWrite.All")
-[datetime]$scriptStartTime                      = Get-Date
-[string]$disableDate = "$(($scriptStartTime).AddDays(-$DeviceDisableThreshold).ToString("yyyy-MM-dd"))T00:00:00z"
-[string]$deleteDate = "$(($scriptStartTime).AddDays(-$DeviceDeleteThreshold).ToString("yyyy-MM-dd"))T00:00:00z"
-if ($Verboselogging){$VerbosePreference         = "Continue"}
-else{$VerbosePreference                         = "SilentlyContinue"}
-#endregion
+    [Parameter(Mandatory = $false)]
+    [int] $DeviceDisableThreshold = 90,
 
-#region ---------------------------------------------------[Functions]------------------------------------------------------------
-function ConnectTo-MgGraph {
-    param (
-        [System.Collections.ArrayList]$RequiredScopes
+    [Parameter(Mandatory = $false)]
+    [int] $DeviceDeleteThreshold = 180,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("ReportOnly", "DisableAndDelete")]
+    [string] $ScriptAction = "ReportOnly"
+)
+
+Import-Module Microsoft.Graph.Authentication
+Import-Module Microsoft.Graph.identity.DirectoryManagement
+
+Disable-AzContextAutosave -Scope Process
+$context = (Connect-AzAccount -Identity).context
+Set-AzContext -SubscriptionName $context.Subscription -DefaultProfile $context
+Connect-MgGraph -NoWelcome
+
+$ErrorActionPreference = "Stop"
+$reportDir = "c:\temp"
+
+function Send-EmailReport {
+<#
+.SYNOPSIS
+Sends an email report using SendGrid.
+
+.DESCRIPTION
+This function sends an email report with the specified content to the given recipient email addresses using SendGrid's API.
+
+.PARAMETER SendGridApiKey
+The API key for authenticating with SendGrid.
+
+.PARAMETER SendGridApiEndpoint
+The endpoint URL for the SendGrid API.
+
+.PARAMETER SenderEmailAddress
+The email address of the sender.
+
+.PARAMETER RecipientEmailAddresses
+An array of recipient email addresses.
+
+.PARAMETER Subject
+The subject of the email.
+
+.PARAMETER Content
+The content of the email.
+
+.PARAMETER Attachments
+An array of attachments to include in the email.
+#>
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string] $SendGridApiKey,
+
+        [Parameter(Mandatory = $true)]
+        [string] $SendGridApiEndpoint,
+
+        [Parameter(Mandatory = $true)]
+        [string] $SenderEmailAddress,
+
+        [Parameter(Mandatory = $true)]
+        [string[]] $RecipientEmailAddresses,
+
+        [Parameter(Mandatory = $true)]
+        [String] $Subject,
+
+        [Parameter(Mandatory = $true)]
+        [String] $Content,
+
+        [Parameter(Mandatory = $true)]
+        [Object[]] $Attachments
     )
-    Begin {
-        $ErrorActionPreference = 'stop'
-        [String]$resourceURL = "https://graph.microsoft.com/"
-        $GraphAccessToken = $null
-        if ($env:AUTOMATION_ASSET_ACCOUNTID) {  [Bool]$ManagedIdentity = $true}  # Check if running in Azure Automation
-        else {                                  [Bool]$ManagedIdentity = $false} # Otherwise running in Local PowerShell
+
+    $headers = @{
+        "Authorization" = "Bearer $SendGridApiKey"
+        "Content-Type"  = "application/json"
+    }
+
+    $attachments = $Attachments | ForEach-Object {
+        $contentCsv = Get-Content $_.file -Raw
+
+        @{
+            content     = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($contentCsv))
+            filename    = [System.IO.Path]::GetFileName($_.file)
+            type        = $_.type
+            disposition = "attachment"
         }
-    Process {
-        if ($ManagedIdentity){ #Connect to the Microsoft Graph using the ManagedIdentity and get the AccessToken
-            Try{$response = [System.Text.Encoding]::Default.GetString((Invoke-WebRequest -UseBasicParsing -Uri "$($env:IDENTITY_ENDPOINT)?resource=$resourceURL" -Method 'GET' -Headers @{'X-IDENTITY-HEADER' = "$env:IDENTITY_HEADER"; 'Metadata' = 'True'}).RawContentStream.ToArray()) | ConvertFrom-Json 
-                $GraphAccessToken = $response.access_token
-                Write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to get an Access Token to Graph for managed identity"
-                }
-            Catch{Write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to get an Access Token to Graph for managed identity, with error: $_"}
-            $GraphVersion = ($GraphVersion = (Get-Module -Name 'Microsoft.Graph.Authentication' -ErrorAction SilentlyContinue).Version | Sort-Object -Desc | Select-Object -First 1)
-            if ('2.0.0' -le $GraphVersion) {
-                Try{Connect-MgGraph -Identity -Nowelcome
-                    $GraphAccessToken = convertto-securestring($response.access_token) -AsPlainText -Force
-                    Write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to connect to Graph with module 2.x and Managedidentity"}
-                Catch{Write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to connect to Graph with module 2.x and Managedidentity, with error: $_"}
-                }
-            else {#Connect to the Microsoft Graph using the AccessToken
-                Try{Connect-mgGraph -AccessToken $GraphAccessToken -NoWelcome
-	                Write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to connect to Graph with module 1.x and Managedidentity"}
-                Catch{Write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to connect to Graph with module 1.x and Managedidentity, with error: $_"}
-                }
-            }
-        else{
-            Try{Connect-MgGraph -Scope $RequiredScopes
-                Write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to connect to Graph manually"}
-            Catch{Write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to connect to Graph manually, with error: $_"}
-            }
-        #Checking if all permissions are granted to the script identity in Graph and exit if not
-        [System.Collections.ArrayList]$CurrentPermissions  = (Get-MgContext).Scopes
-        foreach ($RequiredScope in $RequiredScopes) {
-            if (Compare-Object $currentpermissions $RequiredScope -IncludeEqual | Where-Object -FilterScript {$_.SideIndicator -eq '=='}){
-                Write-Verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success, Script identity has a scope permission: $RequiredScope"
-                }
-            else {Write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed, Script identity is missing a scope permission: $RequiredScope"}
-            }
-        #Return the access token if available and cleanup memory after connecting to Graph
-        return $GraphAccessToken
-        }
-    End {$MemoryUsage = [System.GC]::GetTotalMemory($true)
-        Write-Verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to cleanup Memory usage after connect to Graph to: $(($MemoryUsage/1024/1024).ToString('N2')) MB"
-        }   
+    }
+
+    $body = @{
+        from             = @{ email = $SenderEmailAddress }
+        personalizations = @(@{ to = @($RecipientEmailAddresses | ForEach-Object { @{ email = $_ } }) })
+        subject          = $Subject
+        content          = @(@{ type = "text/plain"; value = $Content })
+        attachments      = $attachments
+    }
+
+    $bodyJson = $body | ConvertTo-Json -Depth 4
+    Invoke-RestMethod -Uri $SendGridApiEndpoint -Method Post -Headers $headers -Body $bodyJson
 }
-#endregion
 
-#region ---------------------------------------------------[[Script Execution]------------------------------------------------------
-$StartTime = Get-Date
-$MgGraphAccessToken = ConnectTo-MgGraph -RequiredScopes $RequiredScopes
+try {
+    $disableDate = [datetime]::UtcNow.AddDays(-$DeviceDisableThreshold).ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $deleteDate = [datetime]::UtcNow.AddDays(-$DeviceDeleteThreshold).ToString("yyyy-MM-ddTHH:mm:ssZ")
 
-#Get Pending Devices to disable
-try{$pendingdevices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $($disableDate) AND ApproximateLastSignInDateTime ge $($deleteDate)"
-    write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to get $($pendingdevices.count) Pending Devices to disable"}
-catch{write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to get Pending Devices with error: $_"}
+    # Fetch Disabled & Deleted Devices
+    $pendingDevices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $disableDate AND ApproximateLastSignInDateTime ge $deleteDate"
+    Write-Verbose "$($pendingDevices.Count) Pending Devices to disable"
 
-#Get Stale Devices to delete
-try{$staledevices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $($deleteDate)"
-    write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to get $($staledevices.count) Stale Devices to delete"}
-catch{write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to get Stale Devices with error: $_"}
+    $staleDevices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $deleteDate"
+    Write-Verbose "$($staleDevices.Count) Stale Devices to delete"
 
-# would it be possible to automate sending these two files (or just variables as a table) via email ? 
-# I noticed there is Sendgrid installed in Azure for sending emails from Azure, could you please rewrite the followin two rows to send email ?
-#recipient would be defined ideally as a variable in automated account, for testing use : jzahumensky@uniphar.ie
+    # Generate CSV Reports
+    $pendingDevices | Export-Csv -Path "$reportDir\disabled-devices.csv" -NoTypeInformation
+    $staleDevices | Export-Csv -Path "$reportDir\deleted-devices.csv" -NoTypeInformation
 
-$staleDevices | Export-Csv -Path c:\temp\deleted-devices.csv -NoTypeInformation
+    # Send Email Report
+    $sendGridApiKey = Get-AzKeyVaultSecret -VaultName $SendGridApiKeyKvName -Name $SendGridApiKeyKvSecretName -AsPlainText
 
-$pendingDevices | Export-Csv -Path c:\temp\disabled-devices.csv -NoTypeInformation
+    $attachments = @(
+        @{
+            file = "$reportDir\disabled-devices.csv"
+            type = "text/csv"
+        }
+        @{
+            file = "$reportDir\deleted-devices.csv"
+            type = "text/csv"
+        }
+    )
 
+    Send-EmailReport -SendGridApiKey $sendGridApiKey `
+                     -SendGridApiEndpoint $SendGridApiEndpoint `
+                     -SenderEmailAddress $SendGridSenderEmailAddress `
+                     -RecipientEmailAddresses $SendGridRecipientEmailAddresses `
+                     -Subject "Device Cleanup Report" `
+                     -Content "Pending Devices to Disable: $($pendingDevices.count), Stale Devices to Delete: $($staleDevices.count)" `
+                     -Attachments $attachments
 
+    # Clean Up Devices
+    if ("DisableAndDelete" -eq $ScriptAction) {
+        $pendingDevices | ForEach-Object {
+            Write-Verbose "Disabling Device $($_.DisplayName)"
+            Update-MgDevice -DeviceId $_.Id -AccountEnabled:$false
+        }
 
-#Disable Pending Devices
-foreach ($device in $pendingdevices) {
-    Write-Output "Device $($device.DisplayName) is pending to be disabled"
-    if ($TestMode -eq $False) {
-        try{Update-MgDevice -DeviceId $device.Id -AccountEnabled:$False
-            write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to disable Device $($device.DisplayName)"}
-        catch{write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to disable Device $($device.DisplayName) with error: $_"}
+        $staleDevices | ForEach-Object {
+            Write-Verbose "Deleting Device $($_.DisplayName)"
+            Remove-MgDevice -DeviceId $_.Id
+        }
     }
 }
-
-#Delete Stale Devices
-foreach ($device in $staledevices) {
-    Write-Output "Device $($device.DisplayName) is stale and will be removed"
-    if ($TestMode -eq $False) {
-        try{Remove-MgDevice -DeviceId $device.Id
-            write-verbose "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Success to remove Device $($device.DisplayName)"}
-        catch{write-Error "$(Get-Date -Format 'yyyy-MM-dd'),$(Get-Date -format 'HH:mm:ss'),Failed to remove Device $($device.DisplayName) with error: $_"}
-    }
+catch {
+    Write-Error $_.Exception.Message
 }
-
-$VerbosePreference = "SilentlyContinue"
