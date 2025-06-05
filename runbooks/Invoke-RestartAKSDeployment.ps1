27d45721953a570f04d06bcb3cf0379d0d3a7f31
VaclavK <vaclav@vaclavkabat.net>
2025-06-05 09:34:22 +0100

    add doco

--- runbooks/Invoke-RestartAKSDeployment.ps1
+++ runbooks/Invoke-RestartAKSDeployment.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+Restarts a specified deployment in an Azure Kubernetes Service (AKS) cluster.
+
+.DESCRIPTION
+This script restarts a specified deployment in an Azure Kubernetes Service (AKS) cluster using the Azure CLI. It requires the resource group name, cluster name, subscription name, namespace, and deployment name as parameters.
+
+.PARAMETER rgName
+The name of the resource group containing the AKS cluster.
+.PARAMETER clusterName
+The name of the AKS cluster.
+.PARAMETER subName
+The name of the Azure subscription containing the AKS cluster.
+.PARAMETER namespace
+The Kubernetes namespace where the deployment is located.
+.PARAMETER deployName
+The name of the deployment to restart.
+.EXAMPLE
+.\Invoke-RestartAKSDeployment.ps1 -rgName "myResourceGroup" -clusterName "myAKSCluster" -subName "mySubscription" -namespace "default" -deployName "myDeployment"
+This example restarts the deployment named "myDeployment" in the "default" namespace of the AKS cluster "myAKSCluster" located in the resource group "myResourceGroup" under the subscription "mySubscription".
+
+#>
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
